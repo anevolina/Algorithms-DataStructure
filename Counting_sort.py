059c773d08af2353625bd15621212faf105d78a2
Nassstasia16 <nassstasia16@gmail.com>
2019-10-02 19:10:01 -0700

    counting sort

---
+++ Counting_sort.py
@@ -0,0 +1,45 @@
+from random import randint
+import time
+
+def counting_sort(array):
+    length = max(array)
+    aux_array = [0 for i in range(length + 1)]
+
+    for i in range(len(array)):
+        aux_array[array[i]] += 1
+
+    for i in range(len(aux_array) - 1):
+        aux_array[i + 1] += aux_array[i]
+
+    result = [0 for i in range(len(array))]
+
+    for item in array:
+        result[aux_array[item]-1] = item
+        aux_array[item] -= 1
+
+    return result
+
+
+"""
+====================================
+Test sort 
+====================================
+"""
+
+
+array = [randint(1, 100) for i in range(10)]
+sorted_array = counting_sort(array)
+
+print('Sort is working correctly:', sorted_array == sorted(array))
+
+cs_time = 0
+for i in range(20):
+    array = [randint(0, 100) for j in range(1000)]
+
+    start = time.time()
+    counting_sorted = counting_sort(array)
+    end = time.time()
+
+    cs_time += end-start
+
+print('Mean execution time for quick sort: ', cs_time/20)
